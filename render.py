e60002f968f72419ff606e54ffeaf4dc99479c81
duducheng <jekyll4168@icloud.com>
2020-11-28 21:11:23 +0800

    automatic processing with GitHub API

---
+++ render.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+import urllib.request
+from collections import namedtuple
+
+Repo = namedtuple("Repo", ["name", "description",
+                           "stars", "forks", "open_issues"])
+
+
+def parse_repo(repo_name: str) -> Repo:
+    print("Parsing {}...".format(repo_name))
+    api = "https://api.github.com/repos/"
+    parsed_json = json.loads(urllib.request.urlopen(api+repo_name).read())
+    repo = Repo(name=parsed_json["full_name"],
+                description=parsed_json["description"],
+                stars=parsed_json["stargazers_count"],
+                forks=parsed_json["forks_count"],
+                open_issues=parsed_json["open_issues_count"])
+    return repo
+
+
+def repo_to_line(repo: Repo) -> str:
+    line_template = "* [{0}](https://github.com/{0}): {1} ![{0} stars](https://img.shields.io/github/stars/{0}) ![{0} forks](https://img.shields.io/github/forks/{0})"
+    return line_template.format(repo.name, repo.description)
+
+
+def parse_repo_block(header: str, repo_names: list) -> tuple:
+    if repo_names:
+        stars = 0
+        forks = 0
+        lines = []
+        for repo_name in repo_names:
+            repo = parse_repo(repo_name)
+            lines.append(repo_to_line(repo))
+            stars += repo.stars
+            forks += repo.forks
+            if repo.open_issues > 0:
+                print("[Warning!] There is open issues: ", repo)
+        return (header + "\n" + "\n".join(lines), stars, forks)
+    return ("", 0, 0)
+
+
+def main(info_path: str, template_path: str, output_path: str = None):
+    with open(info_path) as f:
+        info = json.load(f)
+    with open(template_path) as f:
+        template = f.read()
+
+    bio = info["bio"]
+    preprint = parse_repo_block("### Preprint / Technical Reports",
+                                info["preprint"])
+    pub = parse_repo_block("### Publications", info["pub"])
+    edu = parse_repo_block("## Educational Projects", info["edu"])
+    misc = parse_repo_block("## Misc.", info["misc"])
+
+    output = template.format(bio=bio,
+                             preprint=preprint[0],
+                             pub=pub[0],
+                             edu=edu[0],
+                             misc=misc[0])
+    # print(output)
+
+    total_stars = sum([preprint[1], pub[1], edu[1], misc[1]])
+    total_forks = sum([preprint[2], pub[2], edu[2], misc[2]])
+    print("Total Stars: ", total_stars)
+    print("Total Forks: ", total_forks)
+
+    if output_path is not None:
+        with open(output_path, "w") as f:
+            f.write(output)
+
+
+if __name__ == "__main__":
+    main("info.json", "template.md", "README.md")
---
+++ template.md
@@ -0,0 +1,11 @@
+# Bio.
+{bio}
+
+## Research Projects
+{preprint}
+
+{pub}
+
+{edu}
+
+{misc}
